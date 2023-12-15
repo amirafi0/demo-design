e84d028589da9d773f671f4d056c67596d0fecd9
Rafi <kh9961734@gmail.com>
2023-12-15 09:33:20 +0600

    version1

---
+++ index.js
@@ -0,0 +1,42 @@
+var x;
+x=10;
+
+
+
+document.getElementById('r').innerHTML=x;
+
+
+
+
+let cart=0;
+
+if (19>18) {
+    console.log('u can mary');
+}
+
+
+
+let marks=Number(`78`)
+
+if (marks>=80 ){
+    console.log(`jon get A+`)
+}
+else if (marks>=60  && marks<79){
+    console.log(`jon get A`)
+} 
+else if (marks>50 && marks<59){
+    console.log(`jon get B`)
+}
+
+else {
+    console.log(`jon get F`)
+}
+
+
+
+
+
+
+
+
+
